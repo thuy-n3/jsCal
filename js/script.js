2ea400fe7535a095e55346b536d62fc67a084424
Thuy Nguyen <ngtan3@gmail.com>
2016-10-20 16:52:17 -0500

    querySelecting

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//numbers buttons
+var one = document.querySelector('1')
+var two = document.querySelector('2')
+var three = document.querySelector('3')
+var four = document.querySelector('4')
+var five = document.querySelector('5')
+var six = document.querySelector('6')
+var seven = document.querySelector('7')
+var eight = document.querySelector('8')
+var nine = document.querySelector('9')
+var zero = document.querySelector('0')
+
+//operators 
+var reset = document.querySelector('clear')
+var equal = document.querySelector('.equal')
+var add = document.querySelector('.plus')
+var subtract = document.querySelector('.minus')
+var times = document.querySelector('.multi')
+var divMe = document.querySelector('divide')
+
+//screen 
+var ansScreen = document.querySelector('screen')
+
+
